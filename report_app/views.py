98530cf8027a704fa1011c6169668c5b941e81a1
neomoment-github <neomoment.ai@gmail.com>
2023-02-16 18:18:27 +0530

    new req.txt

--- report_app/views.py
+++ report_app/views.py
@@ -95,12 +95,12 @@ def get_custuser(cname, cadmin, cdepart, chk_true):
 
 def get_connection():
     # # for server
-    conn = pymssql.connect(server='192.168.5.79', user='chatboat_sa', password='ch@tb0@t$@',
+    # conn = pymssql.connect(server='192.168.5.79', user='chatboat_sa', password='ch@tb0@t$@',
     # database='zu_chatbot_log_devel')
-    # conn = pyodbc.connect('Driver=SQL Server;'  ## for local
-    #                       'Server=SOHAIL;'
-    #                       'Database=zayed_university_db;'
-    #                       'Trusted_Connection=yes;')
+    conn = pyodbc.connect('Driver=SQL Server;'  ## for local
+                          'Server=SOHAIL;'
+                          'Database=zayed_university_db;'
+                          'Trusted_Connection=yes;')
     return conn
 
 
